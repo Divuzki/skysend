e0cad89a219d60403e217de58591849057417d0d
Divine Ikhuoria <divuzki@gmail.com>
2024-05-15 14:07:50 +0100

    chore: Remove commented out admin URL in urls.py

--- skysend/urls.py
+++ skysend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import send_email
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", send_email, name="send_email"),
 ]
